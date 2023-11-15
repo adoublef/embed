c3bf53913014dbf72b7696899bac48833897855a
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-15 23:50:15 +0000

    add index scripts

--- internal/network/http/service.go
+++ internal/network/http/service.go
@@ -1,27 +1,34 @@
 package http
 
 import (
-	"fmt"
+	"embed"
 	"net/http"
 
 	"github.com/adoublef/embed/nats"
 	sql "github.com/adoublef/embed/sqlite3"
+	t "github.com/adoublef/embed/template"
 )
 
+//go:embed all:*.html
+var embedFS embed.FS
+var T = t.NewFS(embedFS, "*.html")
+
 type Service struct {
-	m *http.ServeMux
+	m  *http.ServeMux
 	kv *nats.KV
 	db *sql.DB
+	t  t.Template
 }
 
-func (s*Service) ServeHTTP(w http.ResponseWriter, r*http.Request) {
+func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.m.ServeHTTP(w, r)
 }
 
 // A New Service will be created
-func New(db *sql.DB, kv *nats.KV) *Service {
+func New(t t.Template, db *sql.DB, kv *nats.KV) *Service {
 	s := Service{
-		m: http.NewServeMux(),
+		m:  http.NewServeMux(),
+		t:  t,
 		db: db,
 		kv: kv,
 	}
@@ -31,6 +38,6 @@ func New(db *sql.DB, kv *nats.KV) *Service {
 
 func (s *Service) routes() {
 	s.m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "network")
+		t.ExecuteHTTP(w, s.t, "index.html", nil)
 	})
 }
--- internal/whiteboard/http/service.go
+++ internal/whiteboard/http/service.go
@@ -1,17 +1,23 @@
 package http
 
 import (
-	"fmt"
+	"embed"
 	"net/http"
 
 	"github.com/adoublef/embed/nats"
 	sql "github.com/adoublef/embed/sqlite3"
+	t "github.com/adoublef/embed/template"
 )
+
+//go:embed all:*.html
+var embedFS embed.FS
+var T = t.NewFS(embedFS, "*.html")
 
 type Service struct {
 	m  *http.ServeMux
 	kv *nats.KV
 	db *sql.DB
+	t  t.Template
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -19,9 +25,10 @@ func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 // A New Service will be created
-func New(db *sql.DB, kv *nats.KV) *Service {
+func New(t t.Template, db *sql.DB, kv *nats.KV) *Service {
 	s := Service{
-		m: http.NewServeMux(),
+		m:  http.NewServeMux(),
+		t:  t,
 		db: db,
 		kv: kv,
 	}
@@ -31,6 +38,6 @@ func New(db *sql.DB, kv *nats.KV) *Service {
 
 func (s *Service) routes() {
 	s.m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "whiteboard")
+		t.ExecuteHTTP(w,s.t, "index.html", nil)
 	})
 }
